4d8e74afc94cbe54474d7dc9b9cbe65a149cc5e9
milind <godemilind@gmail.com>
2020-05-29 18:24:13 +0530

    modified student

--- student-mnagement-system-ui/src/app/app.component.ts
+++ student-mnagement-system-ui/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas,);
+import { faTachometerAlt,faCog, faFolder,faWrench,faChartArea,faTable,faBars,faLaughWink, faSearch, 
+  faBell,faFileAlt,faDonate,faExclamationTriangle,faEnvelope,faUser,faCogs, faList, faSignOutAlt, faDownload} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faTachometerAlt=faTachometerAlt;
+  faCog=faCog;
+  faFolder=faFolder;
+  faWrench=faWrench;
+  faChartArea=faChartArea;
+  faTable=faTable;
+  faBars=faBars;
+  faLaughWink=faLaughWink;
+  faSearch=faSearch;
+  faBell=faBell;
+  faFileAlt=faFileAlt;
+  faDonate=faDonate;
+  faExclamationTriangle=faExclamationTriangle;
+  faEnvelope=faEnvelope;
+  faUser=faUser;
+  faCogs=faCogs;
+  faList=faList;
+  faSignOutAlt=faSignOutAlt;
+  faDownload=faDownload;
   title = 'student-mnagement-system-ui';
 }
